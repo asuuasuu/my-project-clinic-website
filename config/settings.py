d1bbdc1ee515c6c58e9e2d36528ec1a5b766c984
Vladyslav <asuu12333@gmail.com>
2026-08-06 13:05:26 +0300

    delete from email.charset import BASE64

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
-from email.charset import BASE64
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
