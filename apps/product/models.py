e12e8743777c1798fa2acf04782a2b15dfd39b78
Vladyslav <asuu12333@gmail.com>
2026-08-08 15:20:30 +0300

    хз

--- apps/product/models.py
+++ apps/product/models.py
@@ -18,16 +18,16 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-      name = models.CharField(max_length = 100, blank = False)
-      category = models.ForeignKey(Category, blank = False, on_delete = models.CASCADE)
-      price = models.DecimalField(max_digits = 10, decimal_places = 2)
-      description = models.TextField(blank = False, default='')
+      name               = models.CharField(max_length = 100, blank = False)
+      category           = models.ForeignKey(Category, blank = False, on_delete = models.CASCADE)
+      price              = models.DecimalField(max_digits = 10, decimal_places = 2)
+      description        = models.TextField(blank = False, default='')
       main_product_image = models.ImageField(blank = False, upload_to = 'product/main/')
-      created_at = models.DateTimeField(auto_now_add = True)
-      updated_at = models.DateTimeField(auto_now = True)
-      ingredients = models.TextField(blank = False, default='')
-      slug = models.CharField(max_length = 100, unique = True, blank = True)
-      quantity = models.PositiveIntegerField(default = 0)
+      created_at         = models.DateTimeField(auto_now_add = True)
+      updated_at         = models.DateTimeField(auto_now = True)
+      ingredients        = models.TextField(blank = False, default='')
+      slug               = models.CharField(max_length = 100, unique = True, blank = True)
+      quantity           = models.PositiveIntegerField(default = 0)
 
       def save(self, *args, **kwargs):
             if not self.slug:
@@ -39,5 +39,5 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
-      product = models.ForeignKey(Product, blank = False, on_delete = models.CASCADE)
+      product     = models.ForeignKey(Product, blank = False, on_delete = models.CASCADE)
       extra_image = models.ImageField(upload_to = 'product/extra/')
